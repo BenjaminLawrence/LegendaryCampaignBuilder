5902c90c409f421a5461c1397d8efd7e23d389f2
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-06-20 20:25:25 +0800

    Create Mastermind class

---
+++ LegendaryCampaignBuilder/Mastermind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryCampaignBuilder
+{
+    internal class Mastermind
+    {
+        string Name { get; }
+        int Tier { get; }
+        Difficulty Difficulty { get; }
+        HeroTeam[] TeamsThatMatter { get; }
+        HeroClass[] ClassesThatMatter { get; }
+    }
+}
