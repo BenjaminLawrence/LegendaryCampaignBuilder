9bf689e4a1319fd0e1e3fb21b515bb2539a87448
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-06-20 20:17:02 +0800

    Create HeroTeam enumeration

---
+++ LegendaryCampaignBuilder/HeroTeam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryCampaignBuilder
+{
+    enum HeroTeam
+    {
+        None,
+        Avengers,
+        Brotherhood,
+        Cabal,
+        Champions,
+        CrimeSyndicate,
+        FantasticFour,
+        FoesOfAsgard,
+        GuardiansOfTheGalaxy,
+        HeroesOfAsgard,
+        Illuminati,
+        MarvelKnights,
+        NewWarriors,
+        PlanetHulk,
+        Shield,
+        SinisterSix,
+        SpiderFriends,
+        Venomverse,
+        XForce,
+        XMen,
+    }
+}
