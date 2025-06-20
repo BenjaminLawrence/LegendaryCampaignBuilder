4123ef7b2ffc688ed6c6f5393c292089b263151c
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-06-20 19:56:17 +0800

    Create base hero class

---
+++ LegendaryCampaignBuilder/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryCampaignBuilder
+{
+    internal class Hero
+    {
+        string Name { get; }
+        int Tier { get; }
+        CompatibleCampaigns Campaigns {get;}
+        HeroFaction Faction { get; }
+        Boolean FactionDependent { get; }
+        HeroClass[] Classes { get; }
+        Boolean ClassDependent { get; }
+        Boolean IsPure { get; }
+        Boolean CanKO { get; }
+        Boolean CanHeal { get; }
+        Boolean CanWound { get; }
+    }
+}
--- LegendaryCampaignBuilder/obj/Debug/net8.0/LegendaryCampaignBuilder.AssemblyInfo.cs
+++ LegendaryCampaignBuilder/obj/Debug/net8.0/LegendaryCampaignBuilder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegendaryCampaignBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b2525e4973854aabfea8fe957bb8bdfe8b0292")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegendaryCampaignBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegendaryCampaignBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
