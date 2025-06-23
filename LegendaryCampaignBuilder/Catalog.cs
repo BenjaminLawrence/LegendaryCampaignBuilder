d952272e884a28572e5102cbdf11416ab7581e61
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-06-23 14:02:58 +0800

    Updated Catalog syntax for hero upload

--- LegendaryCampaignBuilder/Catalog.cs
+++ LegendaryCampaignBuilder/Catalog.cs
@@ -10,181 +10,176 @@ namespace LegendaryCampaignBuilder
     internal class Catalog
     {
         private List<Hero> _heroList = new List<Hero>();
+        private List<Mastermind> _mastermindList = new List<Mastermind>();
+        private List<Scheme> _schemeList = new List<Scheme>();
+        private List<VillainGroup> _villainGroupList = new List<VillainGroup>();
+        private List<Henchmen> _henchmenList = new List<Henchmen>();
 
-        public HeroCatalog(Expansions expansions)
+        public Catalog(Expansions expansions)
         {
             if ((expansions & Expansions.Core) != 0)
-                _heroList.AddRange(GetCoreHeroes());
+                AddCoreSet();
             if ((expansions & Expansions.FantasticFour) != 0)
-                _heroList.AddRange(GetCoreHeroes());
+                AddFantasticFourSet();
             if ((expansions & Expansions.DarkCity) != 0)
-                _heroList.AddRange(GetDarkCityHeroes());
+                AddDarkCitySet();
             if ((expansions & Expansions.Villains) != 0)
-                _heroList.AddRange(GetVillainsHeroes());
+                AddVillainsSet();
             if ((expansions & Expansions.PaintTheTownRed) != 0)
-                _heroList.AddRange(GetPaintTheTownRedHeroes());
+                AddPaintTheTownRedSet();
             if ((expansions & Expansions.GOTG) != 0)
-                _heroList.AddRange(GetGOTGHeroes());
+                AddGOTGSet();
             if ((expansions & Expansions.FearItself) != 0)
-                _heroList.AddRange(GetFearItselfHeroes());
+                AddFearItselfSet();
             if ((expansions & Expansions.SecretWarsOne) != 0)
-                _heroList.AddRange(GetSecretWarsOneHeroes());
+                AddSecretWarsOneSet();
             if ((expansions & Expansions.SecretWarsTwo) != 0)
-                _heroList.AddRange(GetSecretWarsTwoHeroes());
+                AddSecretWarsTwoSet();
         }
 
         public List<Hero> GetHeroes() { return _heroList; }
 
-        public List<Hero> GetCoreHeroes()
+        private void AddCoreSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Black Widow", 2, HeroTeam.Avengers, (HeroClass)17));
-            heroes.Add(new Hero("Captain America", 2, HeroTeam.Avengers, (HeroClass)27));
-            heroes.Add(new Hero("Cyclops", 1, HeroTeam.XMen, (HeroClass)12));
-            heroes.Add(new Hero("Deadpool", 1, HeroTeam.None, (HeroClass)19));
-            heroes.Add(new Hero("Emma Frost", 1, HeroTeam.XMen, (HeroClass)15));
-            heroes.Add(new Hero("Gambit", 1, HeroTeam.XMen, (HeroClass)7));
-            heroes.Add(new Hero("Hawkeye", 1, HeroTeam.Avengers, (HeroClass)18));
-            heroes.Add(new Hero("Hulk", 1, HeroTeam.Avengers, (HeroClass)10));
-            heroes.Add(new Hero("Iron Man", 1, HeroTeam.Avengers, (HeroClass)20));
-            heroes.Add(new Hero("Nick Fury", 1, HeroTeam.Shield, (HeroClass)25));
-            heroes.Add(new Hero("Rogue", 1, HeroTeam.XMen, (HeroClass)9));
-            heroes.Add(new Hero("Spider-Man", 1, HeroTeam.SpiderFriends, (HeroClass)27));
-            heroes.Add(new Hero("Storm", 1, HeroTeam.XMen, (HeroClass)6));
-            heroes.Add(new Hero("Thor", 1, HeroTeam.Avengers, (HeroClass)12));
-            heroes.Add(new Hero("Wolverine", 1, HeroTeam.XMen, (HeroClass)2));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Black Widow", 2, HeroTeam.Avengers, (HeroClass)17));
+            _heroList.Add(new Hero("Captain America", 2, HeroTeam.Avengers, (HeroClass)27));
+            _heroList.Add(new Hero("Cyclops", 1, HeroTeam.XMen, (HeroClass)12));
+            _heroList.Add(new Hero("Deadpool", 1, HeroTeam.None, (HeroClass)19));
+            _heroList.Add(new Hero("Emma Frost", 1, HeroTeam.XMen, (HeroClass)15));
+            _heroList.Add(new Hero("Gambit", 1, HeroTeam.XMen, (HeroClass)7));
+            _heroList.Add(new Hero("Hawkeye", 1, HeroTeam.Avengers, (HeroClass)18));
+            _heroList.Add(new Hero("Hulk", 1, HeroTeam.Avengers, (HeroClass)10));
+            _heroList.Add(new Hero("Iron Man", 1, HeroTeam.Avengers, (HeroClass)20));
+            _heroList.Add(new Hero("Nick Fury", 1, HeroTeam.Shield, (HeroClass)25));
+            _heroList.Add(new Hero("Rogue", 1, HeroTeam.XMen, (HeroClass)9));
+            _heroList.Add(new Hero("Spider-Man", 1, HeroTeam.SpiderFriends, (HeroClass)27));
+            _heroList.Add(new Hero("Storm", 1, HeroTeam.XMen, (HeroClass)6));
+            _heroList.Add(new Hero("Thor", 1, HeroTeam.Avengers, (HeroClass)12));
+            _heroList.Add(new Hero("Wolverine", 1, HeroTeam.XMen, (HeroClass)2));
         }
 
-        public List<Hero> GetFantasticFourHeroes()
+        private void AddFantasticFourSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Human Torch"));
-            heroes.Add(new Hero("Invisible Woman"));
-            heroes.Add(new Hero("Mr. Fantastic"));
-            heroes.Add(new Hero("Silver Surfer"));
-            heroes.Add(new Hero("Thing"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Human Torch"));
+            _heroList.Add(new Hero("Invisible Woman"));
+            _heroList.Add(new Hero("Mr. Fantastic"));
+            _heroList.Add(new Hero("Silver Surfer"));
+            _heroList.Add(new Hero("Thing"));
         }
 
-        public List<Hero> GetDarkCityHeroes()
+        private void AddDarkCitySet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Angel", 1, HeroTeam.XMen, (HeroClass)11));
-            heroes.Add(new Hero("Bishop", 1, HeroTeam.XMen, (HeroClass)21));
-            heroes.Add(new Hero("Blade", 1, HeroTeam.MarvelKnights, (HeroClass)27));
-            heroes.Add(new Hero("Cable", 1, HeroTeam.XForce, (HeroClass)21));
-            heroes.Add(new Hero("Colossus", 1, HeroTeam.XForce, (HeroClass)9));
-            heroes.Add(new Hero("Daredevil", 1, HeroTeam.MarvelKnights, (HeroClass)11));
-            heroes.Add(new Hero("Domino", 2, HeroTeam.XForce));
-            heroes.Add(new Hero("Elektra", 1, HeroTeam.MarvelKnights));
-            heroes.Add(new Hero("Forge", 1, HeroTeam.XForce));
-            heroes.Add(new Hero("Ghost Rider", 1, HeroTeam.MarvelKnights));
-            heroes.Add(new Hero("Iceman", 2, HeroTeam.XMen));
-            heroes.Add(new Hero("Iron Fist", 1, HeroTeam.MarvelKnights));
-            heroes.Add(new Hero("Jean Grey", 1, HeroTeam.XMen));
-            heroes.Add(new Hero("Nightcrawler", 1, HeroTeam.XMen));
-            heroes.Add(new Hero("Professor X", 1, HeroTeam.XMen));
-            heroes.Add(new Hero("Punisher", 1, HeroTeam.MarvelKnights));
-            heroes.Add(new Hero("Wolverine (X-Force)", 1, HeroTeam.XForce));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Angel", 1, HeroTeam.XMen, (HeroClass)11));
+            _heroList.Add(new Hero("Bishop", 1, HeroTeam.XMen, (HeroClass)21));
+            _heroList.Add(new Hero("Blade", 1, HeroTeam.MarvelKnights, (HeroClass)27));
+            _heroList.Add(new Hero("Cable", 1, HeroTeam.XForce, (HeroClass)21));
+            _heroList.Add(new Hero("Colossus", 1, HeroTeam.XForce, (HeroClass)9));
+            _heroList.Add(new Hero("Daredevil", 1, HeroTeam.MarvelKnights, (HeroClass)11));
+            _heroList.Add(new Hero("Domino", 2, HeroTeam.XForce));
+            _heroList.Add(new Hero("Elektra", 1, HeroTeam.MarvelKnights));
+            _heroList.Add(new Hero("Forge", 1, HeroTeam.XForce));
+            _heroList.Add(new Hero("Ghost Rider", 1, HeroTeam.MarvelKnights));
+            _heroList.Add(new Hero("Iceman", 2, HeroTeam.XMen));
+            _heroList.Add(new Hero("Iron Fist", 1, HeroTeam.MarvelKnights));
+            _heroList.Add(new Hero("Jean Grey", 1, HeroTeam.XMen));
+            _heroList.Add(new Hero("Nightcrawler", 1, HeroTeam.XMen));
+            _heroList.Add(new Hero("Professor X", 1, HeroTeam.XMen));
+            _heroList.Add(new Hero("Punisher", 1, HeroTeam.MarvelKnights));
+            _heroList.Add(new Hero("Wolverine (X-Force)", 1, HeroTeam.XForce));
         }
 
-        public List<Hero> GetVillainsHeroes()
+        private void AddVillainsSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Bullseye"));
-            heroes.Add(new Hero("Dr. Octopus"));
-            heroes.Add(new Hero("Electro"));
-            heroes.Add(new Hero("Enchantress"));
-            heroes.Add(new Hero("Green Goblin"));
-            heroes.Add(new Hero("Juggernaut"));
-            heroes.Add(new Hero("Kingpin"));
-            heroes.Add(new Hero("Kraven"));
-            heroes.Add(new Hero("Loki"));
-            heroes.Add(new Hero("Magneto"));
-            heroes.Add(new Hero("Mysterio"));
-            heroes.Add(new Hero("Mystique"));
-            heroes.Add(new Hero("Sabretooth"));
-            heroes.Add(new Hero("Ultron"));
-            heroes.Add(new Hero("Venom"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Bullseye"));
+            _heroList.Add(new Hero("Dr. Octopus"));
+            _heroList.Add(new Hero("Electro"));
+            _heroList.Add(new Hero("Enchantress"));
+            _heroList.Add(new Hero("Green Goblin"));
+            _heroList.Add(new Hero("Juggernaut"));
+            _heroList.Add(new Hero("Kingpin"));
+            _heroList.Add(new Hero("Kraven"));
+            _heroList.Add(new Hero("Loki"));
+            _heroList.Add(new Hero("Magneto"));
+            _heroList.Add(new Hero("Mysterio"));
+            _heroList.Add(new Hero("Mystique"));
+            _heroList.Add(new Hero("Sabretooth"));
+            _heroList.Add(new Hero("Ultron"));
+            _heroList.Add(new Hero("Venom"));
         }
 
-        public List<Hero> GetPaintTheTownRedHeroes()
+        private void AddPaintTheTownRedSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Black Cat"));
-            heroes.Add(new Hero("Moon Knight"));
-            heroes.Add(new Hero("Scarlet Spider"));
-            heroes.Add(new Hero("Spider-Woman"));
-            heroes.Add(new Hero("Symbiote Spider-Man"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Black Cat"));
+            _heroList.Add(new Hero("Moon Knight"));
+            _heroList.Add(new Hero("Scarlet Spider"));
+            _heroList.Add(new Hero("Spider-Woman"));
+            _heroList.Add(new Hero("Symbiote Spider-Man"));
         }
 
-        public List<Hero> GetGOTGHeroes()
+        private void AddGOTGSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Drax the Destroyer"));
-            heroes.Add(new Hero("Gamora"));
-            heroes.Add(new Hero("Groot"));
-            heroes.Add(new Hero("Rocket Raccoon"));
-            heroes.Add(new Hero("Star-Lord"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Drax the Destroyer"));
+            _heroList.Add(new Hero("Gamora"));
+            _heroList.Add(new Hero("Groot"));
+            _heroList.Add(new Hero("Rocket Raccoon"));
+            _heroList.Add(new Hero("Star-Lord"));
         }
 
-        public List<Hero> GetFearItselfHeroes()
+        private void AddFearItselfSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Greithoth, Breaker of Wills"));
-            heroes.Add(new Hero("Kuurth, Breaker of Stone"));
-            heroes.Add(new Hero("Nerkkod, Breaker of Oceans"));
-            heroes.Add(new Hero("Nul, Breaker of Worlds"));
-            heroes.Add(new Hero("Skadi"));
-            heroes.Add(new Hero("Skirn, Breaker of Men"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Greithoth, Breaker of Wills"));
+            _heroList.Add(new Hero("Kuurth, Breaker of Stone"));
+            _heroList.Add(new Hero("Nerkkod, Breaker of Oceans"));
+            _heroList.Add(new Hero("Nul, Breaker of Worlds"));
+            _heroList.Add(new Hero("Skadi"));
+            _heroList.Add(new Hero("Skirn, Breaker of Men"));
         }
 
-        public List<Hero> GetSecretWarsOneHeroes()
+        private void AddSecretWarsOneSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Apocalyptic Kitty Pryde"));
-            heroes.Add(new Hero("Black Bolt"));
-            heroes.Add(new Hero("Black Panther"));
-            heroes.Add(new Hero("Captain Marvel"));
-            heroes.Add(new Hero("Dr. Strange"));
-            heroes.Add(new Hero("Lady Thor"));
-            heroes.Add(new Hero("Magik"));
-            heroes.Add(new Hero("Maximus"));
-            heroes.Add(new Hero("Namor"));
-            heroes.Add(new Hero("Old Man Logan"));
-            heroes.Add(new Hero("Proxima Midnight"));
-            heroes.Add(new Hero("Superior Iron Man"));
-            heroes.Add(new Hero("Thanos"));
-            heroes.Add(new Hero("Ultimate Spider-Man"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Apocalyptic Kitty Pryde"));
+            _heroList.Add(new Hero("Black Bolt"));
+            _heroList.Add(new Hero("Black Panther"));
+            _heroList.Add(new Hero("Captain Marvel"));
+            _heroList.Add(new Hero("Dr. Strange"));
+            _heroList.Add(new Hero("Lady Thor"));
+            _heroList.Add(new Hero("Magik"));
+            _heroList.Add(new Hero("Maximus"));
+            _heroList.Add(new Hero("Namor"));
+            _heroList.Add(new Hero("Old Man Logan"));
+            _heroList.Add(new Hero("Proxima Midnight"));
+            _heroList.Add(new Hero("Superior Iron Man"));
+            _heroList.Add(new Hero("Thanos"));
+            _heroList.Add(new Hero("Ultimate Spider-Man"));
         }
 
-        public List<Hero> GetSecretWarsTwoHeroes()
+        private void AddSecretWarsTwoSet()
         {
-            List<Hero> heroes = new List<Hero>();
-            heroes.Add(new Hero("Agent Venom"));
-            heroes.Add(new Hero("Arkon the Magnificent"));
-            heroes.Add(new Hero("Beast"));
-            heroes.Add(new Hero("Black Swan"));
-            heroes.Add(new Hero("The Captain and the Devil"));
-            heroes.Add(new Hero("Captain Britain"));
-            heroes.Add(new Hero("Corvus Glaive"));
-            heroes.Add(new Hero("Dr. Punisher the Soldier Supreme"));
-            heroes.Add(new Hero("Elsa Bloodstone"));
-            heroes.Add(new Hero("Phoenix Force Cyclops"));
-            heroes.Add(new Hero("Ruby Summers"));
-            heroes.Add(new Hero("Shang-Chi"));
-            heroes.Add(new Hero("Silk"));
-            heroes.Add(new Hero("Soulsword Colossus"));
-            heroes.Add(new Hero("Spider-Gwen"));
-            heroes.Add(new Hero("Time-Traveling Jean Grey"));
-            return heroes;
+            // Heroes
+            _heroList.Add(new Hero("Agent Venom"));
+            _heroList.Add(new Hero("Arkon the Magnificent"));
+            _heroList.Add(new Hero("Beast"));
+            _heroList.Add(new Hero("Black Swan"));
+            _heroList.Add(new Hero("The Captain and the Devil"));
+            _heroList.Add(new Hero("Captain Britain"));
+            _heroList.Add(new Hero("Corvus Glaive"));
+            _heroList.Add(new Hero("Dr. Punisher the Soldier Supreme"));
+            _heroList.Add(new Hero("Elsa Bloodstone"));
+            _heroList.Add(new Hero("Phoenix Force Cyclops"));
+            _heroList.Add(new Hero("Ruby Summers"));
+            _heroList.Add(new Hero("Shang-Chi"));
+            _heroList.Add(new Hero("Silk"));
+            _heroList.Add(new Hero("Soulsword Colossus"));
+            _heroList.Add(new Hero("Spider-Gwen"));
+            _heroList.Add(new Hero("Time-Traveling Jean Grey"));
         }
     }
 }
